53f1126be72f4bb4f7ff5293f3f0af723cd71f27
José Antonio González Morente <jgonzalezmorente@gmail.com>
2024-01-16 16:18:30 +0100

    Day 17 - B

--- AOC2023/aoc2023_16.py
+++ AOC2023/aoc2023_16.py
@@ -1,5 +1,6 @@
 import sys
-from typing import Tuple, Dict, Set
+from typing import Tuple, Dict, Set, List
+from copy import deepcopy
 
 sys.setrecursionlimit(10000)
 
@@ -18,17 +19,26 @@ class Point:
         self.directions = set()
 
 class Contraption:
-    points: Dict[ Tuple[int, int], Point ]
+    points: Dict[Tuple[int, int], Point]
     current_position: Tuple[int, int]
+    size: Tuple[int, int]
+
+    @property
+    def total_energized( self ) -> int:
+        return len([ point for point in self.points.values() if point.energized ])
 
     def __init__(self) -> None:
-        self.points = {}        
+        self.points = {}
+        self.size = (0, 0)
     
     def add_point( self, point: Point ) -> None:
         self.points[ ( point.x, point.y ) ] = point
+        if point.x + 1 > self.size[0]:
+            self.size = ( point.x + 1, self.size[1] )
+        if point.y + 1 > self.size[1]:
+            self.size = ( self.size[0], point.y + 1 )
         
-    def energize( self, direction: Tuple[int, int] = (1, 0), current_position: Tuple[int, int] = (-1, 0) ) -> None:
-        
+    def energize( self, direction: Tuple[int, int] = (1, 0), current_position: Tuple[int, int] = (-1, 0) ) -> None:        
         next_position = ( current_position[0] + direction[0], current_position[1] + direction[1] )        
         if not next_position in self.points:
             return
@@ -85,6 +95,7 @@ class Contraption:
             
             case '.':
                 self.energize( direction, next_position )
+    
 
 if __name__ == '__main__':
     
@@ -97,10 +108,25 @@ if __name__ == '__main__':
     with open( file ) as f:
         for j, line in enumerate(f):            
             for i, value in enumerate(line.strip()):
-                contraption.add_point( Point( i, j, value ) )
+                contraption.add_point( Point( i, j, value ) )        
     
     # Part I
-    contraption.energize()
-    print( len([ point for point in contraption.points.values() if point.energized ]) )
+    contraption_copy = deepcopy( contraption )
+    contraption_copy.energize()
+    print( contraption_copy.total_energized )
     
-    # Part 2
+    # Part 2    
+    top   = [ ( ( i, -1 ), ( 0, 1 ) ) for i in range(contraption.size[0] ) ]        
+    down  = [ ( ( i, contraption.size[1] ), ( 0, -1 ) ) for i in range(contraption.size[0] ) ]        
+    left  = [ ( ( -1, i ), ( 1, 0 ) ) for i in range(contraption.size[1] ) ]        
+    right = [ ( ( contraption.size[0], i ), ( -1, 0 ) ) for i in range(contraption.size[1] ) ]
+    
+    border_direction = top + down + left + right
+
+    values: List[int] = []
+    for initial_point, direction in border_direction:
+        contraption_copy = deepcopy( contraption )
+        contraption_copy.energize( direction = direction, current_position = initial_point )
+        values.append( contraption_copy.total_energized )
+    
+    print(max(values) )
