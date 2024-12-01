d1e2b16639f8b14743959a48511b403e0bd8a9c0
jgonzalezmorente <jgonzalezmorente@gmail.com>
2024-12-01 18:30:08 +0100

    Advent of Code 2023 - Day 18 - part II

--- AOC2023/aoc2023_18.py
+++ AOC2023/aoc2023_18.py
@@ -3,34 +3,35 @@ from functools import reduce
 def calc_area(band):
     return reduce(lambda area, i: area + (band[i][0] - band[i-1][0]) * band[i][1] ,range(1, len(band)), 0)
 
-if __name__ == '__main__':
-    inputs = 'inputs/day18.txt'
-    sample = 'samples/day18.txt'
+def get_next_square(current_square, direction, length):
+    match direction:
+        case 'U':
+            return tuple((x, y + length) for (x, y) in current_square)
+        case 'D':
+            return tuple((x, y - length) for (x, y) in current_square)
+        case 'L':
+            return tuple((x - length, y) for (x, y) in current_square)
+        case 'R':
+            return tuple((x + length, y) for (x, y) in current_square)
 
-    file = inputs
+def process_trench(file, p2=False):
     trench = [
         {'square': ((0, 0), (1, 0), (1, 1), (0, 1)), 'color': '', 'direction': '', 'length': 0}
     ]
-
-    # Part I
+    if p2:
+        directions = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
     with open(file) as f:
         for line in f:
-            line = line.strip()
-            direction, length, color = line.split()
+            direction, length, color = line.strip().split()
             length = int(length)
             color = color.strip('()')
+            if p2:
+                direction = directions.get(color[-1])
+                length = int(color[1:-1], 16)
             current_square = trench[-1]['square']
             trench[-1]['direction'] = direction
             trench[-1]['length'] = length
-            match direction:
-                case 'U':
-                    next_square = tuple((x, y + length) for (x, y) in current_square)
-                case 'D':
-                    next_square = tuple((x, y - length) for (x, y) in current_square)
-                case 'L':
-                    next_square = tuple((x - length, y) for (x, y) in current_square)
-                case 'R':
-                    next_square = tuple((x + length, y) for (x, y) in current_square)
+            next_square = get_next_square(current_square, direction, length)
             existing_square = next((t for t in trench if t.get('square') == next_square), None)
             if existing_square:
                 existing_square['color'] = color
@@ -41,44 +42,19 @@ if __name__ == '__main__':
                     'direction': '',
                     'length': 0,
                 })
+    return trench
 
-    band1 = []
-    band2 = []
+def build_bands(trench):
+    band1, band2 = [], []
     for i, t in enumerate(trench):
         p = trench[i - 1]
         match (p['direction'], t['direction']):
-            case('U', 'L'):
+            case ('U', 'L') | ('D', 'R') | ('L', 'U') | ('R', 'D'):
                 a = t['square'][0]
                 b = t['square'][2]
-
-            case('U', 'R'):
+            case ('U', 'R') | ('D', 'L') | ('L', 'D') | ('R', 'U'):
                 a = t['square'][1]
                 b = t['square'][3]
-
-            case('D', 'L'):
-                a = t['square'][1]
-                b = t['square'][3]
-
-            case('D', 'R'):
-                a = t['square'][0]
-                b = t['square'][2]
-
-            case('L', 'U'):
-                a = t['square'][0]
-                b = t['square'][2]
-
-            case('L', 'D'):
-                a = t['square'][1]
-                b = t['square'][3]
-
-            case('R', 'U'):
-                a = t['square'][1]
-                b = t['square'][3]
-
-            case('R', 'D'):
-                a = t['square'][0]
-                b = t['square'][2]
-
         if not band1 and not band2:
             band1.append(a)
             band2.append(b)
@@ -89,10 +65,17 @@ if __name__ == '__main__':
             else:
                 band1.append(b)
                 band2.append(a)
+    return band1, band2
 
+if __name__ == '__main__':
+    inputs = 'inputs/day18.txt'
+    sample = 'samples/day18.txt'
+
+    p2 = True
+
+    file = inputs
+    trench = process_trench(file, p2)
+    band1, band2 = build_bands(trench)
     area = max(calc_area(band1), calc_area(band2))
-    print(area)
-
-
-
+    print(f'Volumen laguna: {area}')
 
